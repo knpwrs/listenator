db0a489c48ee7f7b222782469e829677132c1991
Kenneth Powers <ken@kenpowers.net>
2020-11-07 14:02:57 -0500

    feat: initial implementation

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,47 @@
 import listenator from './';
 
-test('it should foo', async () => {
-  for await (const str of listenator()) {
-    expect(str).toBe('foo');
+test('it should asynchronously yield callback arguments and finish', async () => {
+  const gen = listenator<number>((callback, done) => {
+    let val = 0;
+    const tick = () => {
+      setTimeout(() => {
+        callback(val++);
+        if (val < 3) {
+          tick();
+        } else {
+          done();
+        }
+      }, 10);
+    };
+    tick();
+  });
+
+  let current = 0;
+  let totalValues = 0;
+
+  for await (const num of gen) {
+    expect(num).toBe(current++);
+    totalValues += 1;
   }
+
+  expect(totalValues).toBe(3);
+});
+
+test('it should synchronously yield callback arguments and finish', async () => {
+  const gen = listenator<number>((callback, done) => {
+    callback(0);
+    callback(1);
+    callback(2);
+    done();
+  });
+
+  let current = 0;
+  let totalValues = 0;
+
+  for await (const num of gen) {
+    expect(num).toBe(current++);
+    totalValues += 1;
+  }
+
+  expect(totalValues).toBe(3);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,58 @@
-export default async function* () {
-  yield 'foo';
+export type Factory<T> = (
+  emit: (t: T) => unknown,
+  done: () => unknown,
+) => unknown;
+
+function makeDeferral<T = void>() {
+  let resolve: (t: T) => unknown;
+  const promise = new Promise<T>((_res) => {
+    resolve = _res;
+  });
+
+  // @ts-ignore: resolve is not used before it is defined since the code above runs synchronously
+  return [resolve, promise] as const;
+}
+
+async function defer() {
+  const [resolve, promise] = makeDeferral();
+  resolve();
+
+  return promise;
+}
+
+export default async function* listenator<T>(factory: Factory<T>) {
+  // Set up iterator state
+  const queue: Array<T> = [];
+  let done = false;
+  let [resolve, prom] = makeDeferral();
+
+  // Call the factory function with state modifying functions
+  factory(
+    // emit
+    (t: T) => {
+      queue.push(t);
+      resolve();
+    },
+    // done
+    () => {
+      done = true;
+      resolve();
+    },
+  );
+
+  // Asynchronous iterator loop
+  while (true) {
+    await prom;
+    // Flush all queued values
+    while (queue.length > 0) {
+      yield queue.shift();
+      await defer();
+    }
+    // At this point the queue is empty, should we complete the iterator?
+    if (done) {
+      break;
+    }
+    // If we aren't done yet, set up `resolve` and `prom` for the next value
+    [resolve, prom] = makeDeferral();
+  }
 }
