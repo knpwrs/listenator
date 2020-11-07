1d4e6fde7a58722f5f5666cc813aba2c03b5766c
Kenneth Powers <ken@kenpowers.net>
2020-11-07 16:25:37 -0500

    test: add mixed sync/async tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,7 @@
 import listenator from './';
 
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
 test('it should asynchronously yield callback arguments and finish', async () => {
   const gen = listenator<number>((callback, done) => {
     let val = 0;
@@ -32,6 +34,48 @@ test('it should synchronously yield callback arguments and finish', async () => 
     callback(0);
     callback(1);
     callback(2);
+    done();
+  });
+
+  let current = 0;
+  let totalValues = 0;
+
+  for await (const num of gen) {
+    expect(num).toBe(current++);
+    totalValues += 1;
+  }
+
+  expect(totalValues).toBe(3);
+});
+
+test('it should both asynchronously and synchronously yield callback arguments and finish', async () => {
+  const gen = listenator<number>(async (callback, done) => {
+    await delay(10);
+    callback(0);
+    await delay(10);
+    callback(1);
+    callback(2);
+    done();
+  });
+
+  let current = 0;
+  let totalValues = 0;
+
+  for await (const num of gen) {
+    expect(num).toBe(current++);
+    totalValues += 1;
+  }
+
+  expect(totalValues).toBe(3);
+});
+
+test('it should both synchronously and asynchronously yield callback arguments and finish', async () => {
+  const gen = listenator<number>(async (callback, done) => {
+    callback(0);
+    callback(1);
+    await delay(10);
+    callback(2);
+    await delay(10);
     done();
   });
 
